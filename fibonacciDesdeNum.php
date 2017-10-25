16bfb921d7c0c48deaa2dcda4f02c63c396c2352
pjorda96 <pjorda96@gmail.com>
2017-10-25 13:14:50 +0200

    fibonacci

--- fibonacciDesdeNum.php
+++ fibonacciDesdeNum.php
@@ -1,22 +1,19 @@
 <?php
 function fibonacci($num){
-    if ($num==0) return $num;
+    $n1=0;
+    $n2=1;
+    fib($n1,$n2,$num);
+}
+function fib($n1,$n2,$num){
+    if ($num==0) return $n1;
     else {
         $n3=$n1;
         $n1=$n2;
         $n2=$n1+$n3;
         echo $n2." ";
-        $num=$num-1
-        fibonacci($num);
+        $num=$num-1;
+        return fib($n1,$n2,$num);
     }
-}
-function fib($n1,$n2){
-    $n3=$n1;
-    $n1=$n2;
-    $n2=$n1+$n3;
-    echo $n2." ";
-    $num=$num-1
-    fibonacci($num);
 }
 
 $numero=10;
