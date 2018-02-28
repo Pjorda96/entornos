56451ca21804f201658de2503a8f082b4aa3282d
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-02-28 08:58:01 +0100

    cambio

---
+++ Cifrado de mensaje/codificador.php
@@ -0,0 +1,13 @@
+<?php
+$frase="No esta declarada";
+
+$fraseTemp="";
+$fraseCodificada="";
+
+$i=0;
+
+while ($i<strlen($frase)){
+    if($i==strlen($frase)-1){
+      $fraseTemp=$fraseTemp . substr($fraseTemp,i,1)
+    }
+}
